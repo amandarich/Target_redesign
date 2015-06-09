7c79d89210b8e6b32a9d4c5a784fc86b2c07e5bf
amandarich <amandarich823@gmail.com>
2015-06-09 15:29:09 -0400

    carousel added and categories redone

--- js/script.js
+++ js/script.js
@@ -33,3 +33,20 @@ $( function(){
         $( this ).removeClass( "active" ).find( ".lev_2_nav" ).hide();
     } );
 } );
+
+
+
+
+$(document).ready(function() {
+ 
+  $("#owl-demo").owlCarousel({
+ 
+      autoPlay: 3000, //Set AutoPlay to 3 seconds
+ 
+      items : 4,
+      itemsDesktop : [1199,3],
+      itemsDesktopSmall : [979,3]
+ 
+  });
+ 
+});
