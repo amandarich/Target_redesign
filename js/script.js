d57ddd09e15fee58697a733ee9f566214774445c
amandarich <amandarich823@gmail.com>
2015-06-10 09:39:44 -0400

    project

--- js/script.js
+++ js/script.js
@@ -7,32 +7,169 @@ $( function(){
     var $menu_button = $( ".main_nav_container" ),
         $nav         = $( ".top_nav" ),
         $lev1_nav    = $( ".top_nav > li" ),
-        $lev2_nav    = $( ".lev_2_nav" );
+        $lev2_nav    = $( ".lev_2_nav" ),
+        breakpoint   = 1024,
+        currentWidth = $( window ).width();
+        // $menu_button_mobile = $( ".main_nav .button" );
+        // $nav_buttons = $( ".main_nav li" );
+        // $nav_list = $( ".main_nav ul" );
+        // $menu_visible = false;
+
+    if( currentWidth > breakpoint ){
 
         $nav.hide();
         $lev2_nav.hide();
+        // $nav_list.hide();
 
+    }
 
     // jQuery( ".menu_button" ).sidr();
 
     $menu_button.on( "mouseenter", function( e ){
-        $nav.show();
+
+        if( currentWidth > breakpoint ){
+
+            $nav.show();
+        }
     } );
 
     $menu_button.on( "mouseleave", function( e ){
-        $nav.hide();
+
+        if( currentWidth > breakpoint ){
+
+            $nav.hide();
+        }
     } );
 
     $lev1_nav.on( "mouseenter", function( e ){
+        
+        if( currentWidth > breakpoint ){
 
-        $( this ).addClass( "active" ).find( ".lev_2_nav" ).show();
+            $( this ).addClass( "active" ).find( ".lev_2_nav" ).show();
+        }
     } );
 
     $lev1_nav.on( "mouseleave", function( e ){
+        
+        if( currentWidth > breakpoint ){
 
-        $( this ).removeClass( "active" ).find( ".lev_2_nav" ).hide();
+            $( this ).removeClass( "active" ).find( ".lev_2_nav" ).hide();
+        }
+    } );
+
+    $( window ).on( "resize", function( e ){
+
+        currentWidth = $( window ).width();
+
+        if( currentWidth > breakpoint ){
+            $nav.hide();
+            $lev2_nav.hide();
+        }
+        else{
+            $nav.show();
+            $lev2_nav.show();
+        }
+    } );
+
+} );
+
+
+/*
+    hide all subnavs on page load
+        only on mobile select subnavs
+        only on mobile hide event
+
+
+    click on title, show subnav
+        click event
+        only on mobile select the correct submenu
+        only on mobile show/hide submenu
+
+    click event on mobile, not desktop
+
+    when page is resized
+        check the page size
+        show if larger than mobile
+        hide if smaller than mobile
+*/
+
+
+
+
+
+
+
+//ready function
+$(function(){
+
+    // select with any valid css selector
+    // selects one element
+    var menu_click   = $( ".dropdown .click" ),
+        sub_nav      = $( ".sub_menu" ),
+        breakpoint   = 1024,
+        currentWidth = $( window ).width();
+
+    if( currentWidth <= breakpoint ){
+        sub_nav.hide();
+    }
+
+    menu_click.on( "click", function( e ){
+
+        if( currentWidth <= breakpoint ){
+
+            $(this).siblings( ".sub_menu" ).toggle();
+        }
+        e.preventDefault();
+    } );
+
+    $( window ).on( "resize", function( e ){
+
+        currentWidth = $( window ).width();
+
+        if( currentWidth > breakpoint ){
+            sub_nav.show();
+        }
+        else{
+            sub_nav.hide();
+        }
     } );
 } );
+
+
+
+
+
+
+// //ready function
+// $(function(){
+
+
+//     //jquery here!
+//     console.log( $ );
+
+
+//     //http://api.jquery.com/ for all jQuery functions
+//     menu_button.addClass( "newClass" );
+
+//     menu_button.on("mousedown", function( e ){
+
+//         if( currentWidth <= breakpoint ){
+
+//             nav_list.slideUp( "slow" );
+//             menu_visible = false;
+
+//         }
+//         else( currentWidth >= breakpoint ){
+//             nav_list.slideDown( "slow" );
+//             menu_visible = true;
+//         }
+
+
+//     } );
+
+
+
+
 
 
 
@@ -50,3 +187,30 @@ $(document).ready(function() {
   });
  
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
